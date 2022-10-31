4ddd80cc20ba44e2cce25b27d1b7dcba774ddb81
AlexAR <moltron_nano@hotmail.com>
2022-10-31 14:24:04 -0600

    feat: add further akka stream testing

--- src/main/scala/playground/CSVReaderTest.scala
+++ src/main/scala/playground/CSVReaderTest.scala
@@ -1,15 +1,12 @@
 package playground
 
+import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, IOResult}
-import akka.stream.scaladsl.{FileIO, Flow, Framing, Sink, Source}
+import akka.stream.{ActorMaterializer, ClosedShape}
+import akka.stream.scaladsl.{Broadcast, FileIO, Flow, Framing, GraphDSL, Merge, RunnableGraph, Sink, Source}
 import akka.util.ByteString
-import akka.util.ByteString.empty.utf8String
-import kantan.csv._
-import kantan.csv.ops._
 
 import java.nio.file.Paths
-import scala.concurrent.Future
 
 object CSVReaderTest extends App {
 
@@ -72,4 +69,32 @@ object CSVReaderTest extends App {
 
   //val graph = source.to(sink)
   //graph.run()
+}
+
+object GraphDSLTest extends App {
+  implicit val actorSystem = ActorSystem("NumberSystem")
+  implicit val materializer = ActorMaterializer()
+
+  val graph = RunnableGraph.fromGraph( GraphDSL.create() {
+    implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
+      val in = Source(1 to 10) //Fan-In Operator
+      val out = Sink.foreach(println) //Fan
+      //Here, bcast is the Broadcast function, which is a Fan-Out function,
+      //and merge is Merge function, which is a Fan-In function.
+      //Fanout Broadcast[T]: (1 input, N outputs) given an input element emits to each output
+      val bcast = builder.add(Broadcast[Int](2))
+      //Merge (N inputs, 1 output) This picks randomly from inputs pushing them one by one to the output
+      val merge = builder.add(Merge[Int](2))
+      val f1, f2, f3, f4 = Flow[Int].map(_ + 1)
+      val f5 = Flow[Int].filter(x => x % 2 == 0)
+      in ~> f1 ~> bcast ~> f2 ~>  merge ~> f4 ~> f5 ~> out
+      bcast ~> f3 ~> merge
+
+      ClosedShape
+  })
+
+  graph.run()
+  actorSystem.terminate()
+
 }
