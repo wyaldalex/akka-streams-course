a8b8b43fdbf1ee54afad482100d3215322c0556c
AlexAR <moltron_nano@hotmail.com>
2022-11-07 10:46:34 -0600

    feat: add graphDSL csv POC test

--- src/main/scala/playground/CSVReaderTest.scala
+++ src/main/scala/playground/CSVReaderTest.scala
@@ -10,6 +10,7 @@ import playground.CSVReaderTest.AccumulatorActorTest.OperationResponse
 
 import scala.concurrent.duration._
 import java.nio.file.Paths
+import java.util.UUID
 
 object CSVReaderTest extends App {
 
@@ -19,7 +20,7 @@ object CSVReaderTest extends App {
                            paymentType: Int, fareAmount: Double, extra: Double, mtaTax: Double,
                            tipAmount: Double, tollsAmount: Double, improvementSurcharge: Double, totalAmount: Double)
 
-  def fromCsvEntryToCaseClass(csvEntry : String): TaxiTripEntry ={
+  def fromCsvEntryToCaseClass(csvEntry: String): TaxiTripEntry = {
     val arrayString = csvEntry.split(",")
     println(s"Trying to parse $csvEntry")
     TaxiTripEntry(
@@ -45,16 +46,27 @@ object CSVReaderTest extends App {
     )
   }
 
+  case class TaxiStat(tripId: String, taxiTripEntry: TaxiTripEntry)
+
+  def addUUID(taxiTripEntry: TaxiTripEntry): TaxiStat = {
+    val uuid = UUID.randomUUID().toString
+    TaxiStat(uuid, taxiTripEntry)
+  }
+
+
   val file = Paths.get("src/main/resources/1ksample.csv")
 
   implicit val system = ActorSystem("principleSystem")
   implicit val materializer = ActorMaterializer()
 
   object AccumulatorActorTest {
-   def props: Props = Props(new AccumulatorActorTest)
-   case class OperationResponse(id: Int)
-   case object PrintResults
+    def props: Props = Props(new AccumulatorActorTest)
+
+    case class OperationResponse(id: Int)
+
+    case object PrintResults
   }
+
   class AccumulatorActorTest extends Actor with ActorLogging {
 
     import AccumulatorActorTest._
@@ -63,8 +75,9 @@ object CSVReaderTest extends App {
     var totalEntries = 0
 
     override def receive: Receive = {
-      case taxiEntry : TaxiTripEntry =>
-        totalAmount += taxiEntry.totalAmount
+      case taxiStat: TaxiStat =>
+        log.info(s"Processing taxi stat  $taxiStat")
+        totalAmount += taxiStat.taxiTripEntry.totalAmount
         totalEntries += 1
         log.info(s"Processing taxi trip entry $totalEntries")
         sender() ! OperationResponse(totalEntries)
@@ -74,43 +87,67 @@ object CSVReaderTest extends App {
         log.info("Wrong messaged delivered")
     }
   }
+
   val accumulatorActorTest = system.actorOf(AccumulatorActorTest.props)
   val accumulatorActorTest2 = system.actorOf(AccumulatorActorTest.props)
 
 
   //sources -> produce stuff
-  val source = FileIO
+  val fileSource = FileIO
     .fromPath(file)
 
   //flow transform
-//  val toStringFlow = Flow[ByteString].map(byteString => {
-//    println("Converting to taxi entry")
-//    val stringVal = byteString.utf8String
-//    fromCsvEntryToCaseClass(stringVal)
-//  })
+  //  val toStringFlow = Flow[ByteString].map(byteString => {
+  //    println("Converting to taxi entry")
+  //    val stringVal = byteString.utf8String
+  //    fromCsvEntryToCaseClass(stringVal)
+  //  })
   val toStringFlow = Framing.delimiter(ByteString("\n"), 256, true).map(_.utf8String)
   val toTaxiEntryFlow = Flow[String].map(fromCsvEntryToCaseClass)
+  val addCommonUUIDFlow = Flow[TaxiTripEntry].map(addUUID)
+  implicit val timeout = Timeout(10 seconds)
 
   //sinks -> consume stuff
   //val sink = Sink.foreach[TaxiTripEntry](x => println(x.toString()))
+
   import AccumulatorActorTest._
+
   val sink = Sink.foreach[OperationResponse](x => println(x.toString()))
+  val sink2 = Sink.foreach[OperationResponse](x => println(x.toString()))
+  val sink3 = Sink.foreach[OperationResponse](x => println(x.toString()))
+  val sink4 = Sink.foreach[OperationResponse](x => println(x.toString()))
 
-  implicit val timeout = Timeout(10 seconds)
+  //Future Flows
+  val actor1Flow = Flow[TaxiStat].mapAsync(parallelism = 1)(event => (accumulatorActorTest ? event).mapTo[OperationResponse]).to(sink)
+  val actor2Flow = Flow[TaxiStat].mapAsync(parallelism = 1)(event => (accumulatorActorTest2 ? event).mapTo[OperationResponse]).to(sink2)
 
-  val runnableGraph = source.via(toStringFlow)
-    .via(toTaxiEntryFlow).mapAsync(parallelism = 1)(event => (accumulatorActorTest ? event ).mapTo[OperationResponse])
-    .to(sink)
-  val runnableGraph2 = source.via(toStringFlow)
-    .via(toTaxiEntryFlow).mapAsync(parallelism = 1)(event => (accumulatorActorTest2 ? event).mapTo[OperationResponse])
-    .to(sink)
-  runnableGraph.run()
-  runnableGraph2.run()
-  accumulatorActorTest ! PrintResults
-  accumulatorActorTest2 ! PrintResults
+//  val runnableGraph = fileSource.via(toStringFlow)
+//    .via(toTaxiEntryFlow).mapAsync(parallelism = 1)(event => (accumulatorActorTest ? event).mapTo[OperationResponse])
+//    .to(sink)
+//  val runnableGraph2 = fileSource.via(toStringFlow)
+//    .via(toTaxiEntryFlow).mapAsync(parallelism = 1)(event => (accumulatorActorTest2 ? event).mapTo[OperationResponse])
+//    .to(sink)
+//  runnableGraph.run()
+//  runnableGraph2.run()
+//  accumulatorActorTest ! PrintResults
+//  accumulatorActorTest2 ! PrintResults
 
   //val graph = source.to(sink)
   //graph.run()
+  val graph = RunnableGraph.fromGraph(GraphDSL.create() {
+    implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
+      val bcast = builder.add(Broadcast[TaxiStat](2))
+      fileSource ~> toStringFlow ~> toTaxiEntryFlow ~> addCommonUUIDFlow ~> bcast.in
+      bcast.out(0) ~> actor1Flow
+      bcast.out(1) ~> actor2Flow
+
+      ClosedShape
+  })
+  graph.run()
+  accumulatorActorTest ! PrintResults
+  accumulatorActorTest2 ! PrintResults
+
 }
 
 object GraphDSLTest extends App {
